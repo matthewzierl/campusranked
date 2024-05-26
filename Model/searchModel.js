e20dd3ad555cab3db916526b126cb80055a17fc8
Matthew Zierl <97222868+matthewzierl@users.noreply.github.com>
2024-05-26 19:21:07 +0900

    Update searchModel.js

--- Model/searchModel.js
+++ Model/searchModel.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 module.exports = function(schoolName){
+    
     let searchSchema = new mongoose.Schema({
         url:String,
         name:String,
